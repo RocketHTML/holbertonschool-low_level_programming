2213e2e2e8ed79a14e0554f2ab68f99de415244e
root <madisononealburke@gmail.com>
2018-01-18 13:30:26 +0000

    lower and uppercase alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int letter = 0;
+
+	while (letter < 26)
+	{
+		putchar('a' + letter);
+		letter++;
+	}
+	letter = 0;
+	while (letter < 26)
+	{
+		putchar('A' + letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
