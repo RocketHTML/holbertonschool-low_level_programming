08206003cf5e188859b66b6d850c2ac73f1fb533
root <madisononealburke@gmail.com>
2018-01-18 13:18:22 +0000

    count the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int letter = 0;
+
+	while (letter < 26)
+	{
+		putchar('a'+letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
