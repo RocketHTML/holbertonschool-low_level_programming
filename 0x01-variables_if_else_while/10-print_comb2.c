3c1afef152aedd60a905ca1d363194d7baa10269
root <madisononealburke@gmail.com>
2018-01-18 14:39:31 +0000

    count to 100

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int right = 48;
+	int left = 48;
+
+	while (left < 58)
+	{
+		while (right < 58)
+		{
+			putchar(right++);
+			putchar(left);
+			if (right == 58)
+			{
+				left++;
+			}
+			if (left < 58)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		right = 48;
+	}
+	putchar('\n');
+	return (0);
+}
