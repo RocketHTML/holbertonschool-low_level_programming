2882d18c0dc436c9efd02317ab62531b1ed12db0
root <madisononealburke@gmail.com>
2018-01-23 08:50:48 +0000

    initial commit

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - entry point
+ * 
+ * Returns - exit code
+ */
+int main()
+{
+	char holbie[9] = {'H','o','l','b','e','r','t','o','n'};
+	char *cp = holbie;
+	for(;cp <holbie+9;cp++)
+	{
+		_putchar(*cp);
+	}
+	putchar('\n');
+	return(0);
+}
