2e72e330aaea7c1bd741e7a2651429dbb8f3f44a
root <madisononealburke@gmail.com>
2018-01-23 10:32:31 +0000

    alphabet function

--- helpers.c
+++ helpers.c
@@ -1,2 +1,25 @@
 #include <stdio.h>
-void _putchar(int x){putchar(x);}
+
+/**
+ * _putchar(int) - outputs chosen character
+ *
+ * Return: void
+ */
+void _putchar(int x)
+{
+	putchar(x);
+}
+
+/**
+ * print_alphabet - prints alphabet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+        int i;
+
+        for(i = 0; i < 26; i++)
+                _putchar(i + 'a');
+        _putchar('\n');
+}
