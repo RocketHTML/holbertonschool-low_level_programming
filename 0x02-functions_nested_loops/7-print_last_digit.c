a738b860f495d1c1bb3a8d0f9170617bc784dd2a
root <madisononealburke@gmail.com>
2018-01-23 14:03:50 +0000

    last digit function

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @n: The character to be parsed
+ *
+ * Return: absolute value of input
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
