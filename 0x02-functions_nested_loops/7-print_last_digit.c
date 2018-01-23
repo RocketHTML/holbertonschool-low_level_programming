5aae6136f05109954e746881b6f5a6bf24d303d5
root <madisononealburke@gmail.com>
2018-01-23 14:07:51 +0000

    last digit function

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,10 +4,10 @@
  * print_last_digit - prints last digit
  * @n: The character to be parsed
  *
- * Return: absolute value of input
+ * Return: the value of the last digit
  */
 int print_last_digit(int n)
 {
-	_putchar(n % 10);
+	_putchar(n % 10 + '0');
 	return (n % 10);
 }
