1f377c48a4fa03c6940b0d6b752d3477db853ce4
root <madisononealburke@gmail.com>
2018-01-23 13:47:28 +0000

    number sign checker function

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,11 +11,20 @@ int print_sign(int n)
 	int result;
 
 	if (n < 0)
+	{
+		_putchar('-');
 		result = -1;
+	}
 	else if (n == 0)
+	{
+		_putchar('0');
 		result = 0;
+	}
 	else
+	{
+		_putchar('+');
 		result = 1;
+	}
 
 	return (result);
 }
