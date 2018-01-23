4fd8f3a734a624231e6276b197a76fe33f611b65
root <madisononealburke@gmail.com>
2018-01-23 11:14:58 +0000

    README update

--- helpers.c
+++ helpers.c
@@ -23,3 +23,21 @@ void print_alphabet(void)
                 _putchar(i + 'a');
         _putchar('\n');
 }
+
+/**
+ * print_alphabet_x10 - prints alphabet ten times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+        int i;
+        int j;
+
+        for (j = 0; j < 10; j++)
+        {
+                for (i = 0; i < 26; i++)
+                        _putchar(i + 'a');
+                _putchar('\n');
+        }
+}
