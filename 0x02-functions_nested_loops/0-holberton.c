c43c963bd4c96d2c9ffa4fe835781e7c206f73d6
root <madisononealburke@gmail.com>
2018-01-23 08:56:24 +0000

    use of header file

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -2,17 +2,18 @@
 
 /**
  * main - entry point
- * 
+ *
  * Returns - exit code
  */
-int main()
+int main(void)
 {
-	char holbie[9] = {'H','o','l','b','e','r','t','o','n'};
+	char holbie[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
 	char *cp = holbie;
-	for(;cp <holbie+9;cp++)
+
+	for (; cp < holbie + 9 ; cp++)
 	{
 		_putchar(*cp);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
