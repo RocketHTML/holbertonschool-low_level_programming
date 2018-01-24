54dcc46f753fa22749f15c9bd5000391563fc177
root <madisononealburke@gmail.com>
2018-01-24 14:25:17 +0000

    sum even numbered fibonacci numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * fib_sum - sums even fib numbers under n
+ *
+ * @n: the upper limit on the fib values to consider
+ *
+ */
+void fib_sum(long int n)
+{
+	long int a = 0, b = 1;
+	long int c = a + b;
+	long int sum = 0;
+
+	while (c <= n)
+	{
+		a = b;
+		b = c;
+		c = a + b;
+		if (c % 2 == 0)
+			sum += c;
+	}
+	printf("%ld\n", c);
+}
+
+/**
+ * main - entry point
+ *
+ * Return: always returns 0 (success)
+ */
+int main(void)
+{
+	fib_sum(4000000);
+	return (0);
+}
