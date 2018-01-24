95a546307f1fcf5e4334c230b32e50512e8655b1
root <madisononealburke@gmail.com>
2018-01-24 15:09:02 +0000

    unsigned long fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * fib98 - finds the nth fibonacci number (which will be 98 in this case)
+ *
+ * @n: the index of a number in the fibonacci sequence
+ *
+ * Return: the nth fibonacci number
+ */
+void fib98(int n)
+{
+	long int depth = 2, a = 0, b = 1;
+	unsigned long int c = a + b;
+
+	printf("%d, ", 1);
+	while (depth <= n)
+	{
+		a = b;
+		b = c;
+		c = a + b;
+		printf("%lu", c);
+		if (depth++ < n)
+			printf(", ");
+	}
+	printf("\n");
+}
+
+/**
+ * main - entry point
+ *
+ * Return: always returns 0 (success)
+ */
+int main(void)
+{
+	fib98(98);
+	return (0);
+}
