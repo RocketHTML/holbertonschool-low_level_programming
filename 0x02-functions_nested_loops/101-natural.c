5a58d24478d4bd0c6bb15423c84928d2549cd245
root <madisononealburke@gmail.com>
2018-01-24 12:08:49 +0000

    print natural multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * natural_sum - adds two numbers
+ *
+ */
+void natural_sum(void)
+{
+	int sum = 0, c5 = 5, c3 = 3;
+
+	while (c5 < 1024)
+	{
+		sum += c5;
+		c5 += 5;
+	}
+	while (c3 < 1024)
+	{
+		sum += c3;
+		c3 += 3;
+	}
+	printf("%d\n", sum);
+}
---
+++ hi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "0x02-functions_nested_loops/holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	natural_sum();
+	return (0);
+}
