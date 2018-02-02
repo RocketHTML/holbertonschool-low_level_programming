556eb981f8b7fa28a4fd7948ec795425d578ee62
root <madisononealburke@gmail.com>
2018-02-02 02:43:56 +0000

    101-magic.c

--- hi.c
+++ hi.c
@@ -8,52 +8,34 @@
  */
 int main(void)
 {
-        char *n = "1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458";
-        char *m = "9034790663470697234682914569346259634958693246597324659762347956349265983465962349569346";
-        char r[100];
-        char r2[10];
-        char r3[11];
-        char *res;
+  int n;
+  int a[5];
+  int *p;
 
-        res = infinite_add(n, m, r, 100);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        n = "1234567890";
-        m = "1";
-        res = infinite_add(n, m, r2, 10);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        n = "999999999";
-        m = "1";
-        res = infinite_add(n, m, r2, 10);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        res = infinite_add(n, m, r3, 11);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        return (0);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("&p    = %p\n", &p);
+  printf("size: %lu\n", sizeof(p));
+  printf("&a[2] - &n = %lu\n\n", (&a[2] - &n));
+  
+  printf("&a[0] = %p\n", &a[0]);
+  printf("size: %lu\n\n", sizeof(a));
+  printf("&a[2] = %p\n", &a[2]);
+  printf("size: %lu\n\n", sizeof(a[2]));
+  printf("&a[4] = %p\n", &a[4]);
+  printf("size: %lu\n\n", sizeof(a[4]));
+  printf("&n    = %p\n", &n);
+  printf("size: %lu\n\n", sizeof(n));
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
