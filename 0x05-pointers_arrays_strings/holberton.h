68443540fa148686659122d5022cfd7d332746b2
root <madisononealburke@gmail.com>
2018-02-02 00:53:07 +0000

    102-infinite_add.c

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -36,4 +36,5 @@ int _strlen(char *s);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 #endif
