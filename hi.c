bc5be55753109d92c7ee5e053253e3e20993aea0
root <madisononealburke@gmail.com>
2018-02-02 03:43:49 +0000

    print number

--- hi.c
+++ hi.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "0x05-pointers_arrays_strings/holberton.h"
+#include <limits.h>
 
 /**
  * main - Entry point
@@ -8,34 +9,15 @@
  */
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
-
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-  *(p + 5) = 98;
-  /* ...so that this prints 98\n */
-  printf("&p    = %p\n", &p);
-  printf("size: %lu\n", sizeof(p));
-  printf("&a[2] - &n = %lu\n\n", (&a[2] - &n));
-  
-  printf("&a[0] = %p\n", &a[0]);
-  printf("size: %lu\n\n", sizeof(a));
-  printf("&a[2] = %p\n", &a[2]);
-  printf("size: %lu\n\n", sizeof(a[2]));
-  printf("&a[4] = %p\n", &a[4]);
-  printf("size: %lu\n\n", sizeof(a[4]));
-  printf("&n    = %p\n", &n);
-  printf("size: %lu\n\n", sizeof(n));
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+	print_number(98);
+    putchar('\n');
+    print_number(INT_MAX);
+    putchar('\n');
+    print_number(INT_MIN);
+    putchar('\n');
+    print_number(0);
+    putchar('\n');
+    print_number(-98);
+    putchar('\n');
+    return (0);
 }
