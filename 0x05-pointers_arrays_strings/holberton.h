611af89d01455da1a7741f767e198fe803246483
root <madisononealburke@gmail.com>
2018-02-03 23:47:48 +0000

    2-strncpy.c

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -44,4 +44,5 @@ char *cap_string(char *);
 char *string_toupper(char *);
 void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
 #endif
