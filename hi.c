f14878e0d01aae9ebcc4e7786cb3e6cf153556a2
root <madisononealburke@gmail.com>
2018-02-05 20:52:02 +0000

    0-memset.c

--- hi.c
+++ hi.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "0x05-pointers_arrays_strings/holberton.h"
+#include "0x06-pointers_arrays_strings/holberton.h"
 #include <limits.h>
 
 /**
@@ -9,15 +9,5 @@
  */
 int main(void)
 {
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *p;
-
-    printf("%s\n", s1);
-    printf("%s", s2);
-    p = _strcat(s1, s2);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", p);
     return (0);
 }
