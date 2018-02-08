01aacca3391913c2d4d72919fa08760c1feac2f2
root <madisononealburke@gmail.com>
2018-02-08 02:24:07 +0000

    100-wildcmp.c

--- hi.c
+++ hi.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include "0x06-pointers_arrays_strings/holberton.h"
-#include <limits.h>
+#include "0x07-recursion/holberton.h"
 
 /**
  * main - Entry point
@@ -9,5 +8,31 @@
  */
 int main(void)
 {
+    int r;
+
+    r = wildcmp("main.c", "*.c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m*a*i*n*.*c*");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "main.c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m*c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "ma****c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "*");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "***");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m.*c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "**.*c");
+    printf("%d\n", r);
+    r = wildcmp("main-main.c", "ma*in.c");
+    printf("%d\n", r);
+    r = wildcmp("main", "main*d");
+    printf("%d\n", r);
+    r = wildcmp("abc", "*b");
+    printf("%d\n", r);
     return (0);
 }
