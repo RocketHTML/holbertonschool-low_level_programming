9cd29ee83660ff0f24c0918c0817935804af7a12
root <madisononealburke@gmail.com>
2018-02-10 22:18:37 +0000

    2-strlen_recursion.c

--- 0x07-recursion/2-strlen_recursion.c
+++ 0x07-recursion/2-strlen_recursion.c
@@ -1,3 +1,5 @@
+#include "holberton.h"
+
 /**
  * _strlen_recursion - returns int
  * @s: char array
@@ -5,6 +7,7 @@
  */
 int _strlen_recursion(char *s)
 {
-
-	return(0);
+	if (*s == 0)
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
