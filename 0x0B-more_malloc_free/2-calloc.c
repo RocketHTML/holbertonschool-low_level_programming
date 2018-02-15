26139aefc6bfc3cba5ab217b8643adf8c2c697b0
root <madisononealburke@gmail.com>
2018-02-15 23:32:52 +0000

    2-calloc.c

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 /**
  * _calloc - returns void
- * @nmemb: unsigned
- * @size: unsigned
+ * @nmemb: unsigned int
+ * @size: unsigned int
  * Return: void pointer
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
@@ -10,12 +10,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *p;
 	unsigned int bytes = nmemb * size;
 
-	if (size == 0 || nmemb == 0)
+	if (size <= 0 || nmemb <= 0)
 		return (0);
 	p = malloc(bytes);
 	if (!p)
 		return (0);
 	for (bytes = bytes - 1; bytes > 0; bytes--)
 		p[bytes] = 0;
-	return (p);
+	p[0] = 0;
+	return ((void *)p);
 }
