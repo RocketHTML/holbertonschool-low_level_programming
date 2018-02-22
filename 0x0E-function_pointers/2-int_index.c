706219d3ec4f4b8ee1a9999a15eeceb8b66833c9
root <madisononealburke@gmail.com>
2018-02-22 00:03:26 +0000

    2-int_index.c

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+/**
+ * int_index - returns int
+ * @array: int array
+ * @size: int
+ * @cmp: int
+ * Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!array || size <= 0 || !cmp)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
