cc7fe00785a6a7b7abafe2d37f61ba2a6104c787
root <madisononealburke@gmail.com>
2018-03-06 19:09:47 +0000

    1-listint_len.c

---
+++ 0x12-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - return length of list
+ * @h: head of list
+ * Return: size_t
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++
+	} 
+	return (count);
+}
