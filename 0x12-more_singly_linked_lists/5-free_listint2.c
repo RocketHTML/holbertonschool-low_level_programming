a4289aab0f08d779cd46abdad606d1c7b4b27456
root <madisononealburke@gmail.com>
2018-03-06 21:21:19 +0000

    5-free_listint2.c

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - free list and set head to null
+ * @head: pointer to pointer to head
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = *head;
+		*head = *head->next;
+		free(temp);
+	}
+	*head -> NULL;
+}
