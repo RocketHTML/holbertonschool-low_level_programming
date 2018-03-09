a45f52a3fa9076cfd9686db2f023fe4550d051e5
root <madisononealburke@gmail.com>
2018-03-09 19:23:31 +0000

    3-set_bit.c

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -11,7 +11,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 	if (!n)
 		return (-1);
-	if (index > (sizeof(n) - 1))
+	if (index > (sizeof(n) * 8 - 1))
 		return (-1);
 	while (index)
 	{
