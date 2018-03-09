7da6c762fc30623f574935104ae8f343bd59b602
root <madisononealburke@gmail.com>
2018-03-09 09:18:38 +0000

    4-clear_bit.c

---
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * clear_bit - clears a bit
+ * @n: number to change
+ * @index: index of bit to change
+ * Return: 1 if success 0 otherwise
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+	unsigned long int mask = 1;
+
+	if (!n)
+		return (-1);
+	while (index)
+	{
+		mask *= 2;
+		index--;
+	}
+	mask = ~mask;
+	*n = *n & mask;
+	return (1);
+}
