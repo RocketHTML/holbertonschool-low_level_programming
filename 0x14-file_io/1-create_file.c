9835851d3d6ddcd7dbb05383f2269fd3b6c39acd
root <madisononealburke@gmail.com>
2018-03-14 04:06:06 +0000

    1-create_file.c

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -1,4 +1,18 @@
 #include "holberton.h"
+
+/**
+ * len - string length
+ * @s: string
+ * Return: lenth
+ */
+int len(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
 /**
  * create_file - creates a file
  * @filename: name of file to create
@@ -7,6 +21,20 @@
  */
 int create_file(const char *filename, char *text_content)
 {
+	int fd;
+	int write_status;
 
-	return (0);
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	write_status = write(fd, text_content, len(text_content));
+	if (!write_status)
+		return (-1);
+	close(fd);
+	return (1);
 }
