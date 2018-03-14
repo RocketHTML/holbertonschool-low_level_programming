d4260a194853fd5d79a9b29b637e3c6fcd2a8c24
root <madisononealburke@gmail.com>
2018-03-14 01:01:18 +0000

    0-read_textfile.c

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ * read_textfile - reads an amount from textfile, prints to POSIX stdout
+ * @filename: string filename
+ * @letters: number of characters to read and print
+ * Return: number of letters it could print, or 0 if failed or empty
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	int read_status;
+	int write_status;
+
+	if (!filename)
+		return (0);
+
+	buf = malloc(letters);
+	if (!buf)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
+	read_status = read(fd, buf, letters);
+	if (read_status <= 0)
+		return (0);
+	close(fd);
+
+	write_status = write(STDOUT_FILENO, buf, read_status);
+	if (!write_status)
+		return (0);
+	free(buf);
+	return (write_status);
+}
