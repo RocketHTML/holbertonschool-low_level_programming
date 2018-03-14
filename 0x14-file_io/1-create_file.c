ca26898b15b0d00b5ebc50a9eee40805755cf9e8
root <madisononealburke@gmail.com>
2018-03-14 04:16:51 +0000

    1-create_file.c

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -30,10 +30,12 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fd == -1)
 		return (-1);
+
 	if (!text_content)
 		text_content = "";
+
 	write_status = write(fd, text_content, len(text_content));
-	if (!write_status)
+	if (write_status == -1)
 		return (-1);
 
 	close(fd);
