e96c98de69bc268c7f913b808126c5ef6a788264
root <madisononealburke@gmail.com>
2018-05-12 00:05:57 +0000

    set all to null

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -9,6 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
 	hash_node_t **array = NULL;
+	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
@@ -20,6 +21,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table->array = array;
 	table->size = size;
+
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
 
 	return (table);
 }
