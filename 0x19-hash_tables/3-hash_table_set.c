6310fd8925489c6a11ef80ae46f5b08220b36419
root <madisononealburke@gmail.com>
2018-05-12 02:32:45 +0000

    set element in table

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -13,26 +13,30 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int idx;
 	hash_node_t *runner = NULL;
 
-	if (ht == NULL || key == NULL || strlen(key) == 0)
+	if (ht == NULL || ht->size == 0 || key == NULL || strlen(key) == 0)
 		return (0);
+
+	idx = key_index((unsigned char *)key, ht->size);
+
+
+	runner = ht->array[idx];
+	while (runner)
+	{
+		if (strcmp(runner->value, value) == 0)
+		{
+			free(runner->value);
+			runner->value = strdup(value);
+			return (1);
+		}
+		runner = runner->next;
+	}
 
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
 	node->key = strdup(key);
 	node->value = strdup(value);
-	node->next = NULL;
-
-	idx = key_index((unsigned char *)key, ht->size);
-
-	if (ht->array[idx] == NULL)
-		ht->array[idx] = node;
-	else
-	{
-		runner = ht->array[idx];
-		node->next = runner;
-		ht->array[idx] = node;
-	}
-
+	node->next = ht->array[idx];
+	ht->array[idx] = node;
 	return (1);
 }
