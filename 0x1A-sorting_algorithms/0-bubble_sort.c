815095777f0bf86470a3dd43122f7b7de6b9da91
root <madisononealburke@gmail.com>
2018-05-29 18:31:07 +0000

    add flag for optimal speed

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -11,8 +11,9 @@ void bubble_sort(int *array, size_t size)
 	size_t i = 0;
 	int hold;
 	int hold2;
+	int flag = 0;
 
-	if (!array || size == 1)
+	if (!array)
 		return;
 
 	for (j = size - 1; j > 0; j--)
@@ -21,6 +22,7 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[i] > array[i + 1])
 			{
+				flag = 1;
 				hold = array[i];
 				hold2 = array[i + 1];
 				array[i] = hold2;
@@ -28,5 +30,8 @@ void bubble_sort(int *array, size_t size)
 				print_array(array, size);
 			}
 		}
+		if (flag == 0)
+			break;
+		flag = 0;
 	}
 }
