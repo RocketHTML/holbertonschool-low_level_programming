ea5cd94658a862316ace8b4f98f748cdb6e7fe02
root <madisononealburke@gmail.com>
2018-05-29 18:31:49 +0000

    remove flag

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -11,7 +11,6 @@ void bubble_sort(int *array, size_t size)
 	size_t i = 0;
 	int hold;
 	int hold2;
-	int flag = 0;
 
 	if (!array)
 		return;
@@ -22,7 +21,6 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[i] > array[i + 1])
 			{
-				flag = 1;
 				hold = array[i];
 				hold2 = array[i + 1];
 				array[i] = hold2;
@@ -30,8 +28,5 @@ void bubble_sort(int *array, size_t size)
 				print_array(array, size);
 			}
 		}
-		if (flag == 0)
-			break;
-		flag = 0;
 	}
 }
