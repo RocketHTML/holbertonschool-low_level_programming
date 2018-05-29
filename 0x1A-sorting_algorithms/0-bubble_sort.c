559e18b92758dac80b57d9edd9351849be2550b8
root <madisononealburke@gmail.com>
2018-05-29 17:43:40 +0000

    bubble sort

---
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts bubbles
+ * @array: pointer to array
+ * @size: size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t j = 0;
+	size_t i = 0;
+	int hold;
+	int hold2;
+
+	if (!array)
+		return;
+
+	for (j = size - 1; j > 0; j--)
+	{
+		for (i = 0; i < j; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				hold = array[i];
+				hold2 = array[i + 1];
+				array[i] = hold2;
+				array[i + 1] = hold;
+			}
+		}
+	}
+}
