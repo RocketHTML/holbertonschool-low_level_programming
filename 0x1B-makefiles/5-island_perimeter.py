bc53c8014f7807922f374886819b8bd79d7c88f1
root <madisononealburke@gmail.com>
2018-06-16 07:09:11 +0000

    check edges

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -6,13 +6,15 @@ def island_perimeter(grid):
 
     def liberty(grid, i, j):
         '''returns the value at index i j'''
+        if (i < 0 or j < 0):
+            return 0
         if (i >= len(grid) or j >= len(grid[i])):
             return 0
         return grid[i][j]
 
     perimeter = 0
-    for i in range(1, len(grid)):
-        for j in range(1, len(grid[i])):
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid[i])):
             if (grid[i][j] == 1):
                 liberties = 0
                 liberties += 1 - liberty(grid, i - 1, j)
