9c95d80f8c8f4641c48368369de066e194d7abc1
root <madisononealburke@gmail.com>
2018-06-16 06:56:44 +0000

    check if standing on land before adding

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 
+
 def island_perimeter(grid):
     '''returns perimeter of an island'''
     perimeter = 0
     for i in range(1, len(grid) - 1):
-        for j in range(1, len (grid) - 1):
-            liberties = 0
-            liberties += 1 - grid[i - 1][j]
-            liberties += 1 - grid[i][j - 1]
-            liberties += 1 - grid[i + 1][j]
-            liberties += 1 - grid[i][j + 1]
-            perimeter += liberties
+        for j in range(1, len(grid) - 1):
+            if (grid[i][j] == 1):
+                liberties = 0
+                liberties += 1 - grid[i - 1][j]
+                liberties += 1 - grid[i][j - 1]
+                liberties += 1 - grid[i + 1][j]
+                liberties += 1 - grid[i][j + 1]
+                perimeter += liberties
     return perimeter
