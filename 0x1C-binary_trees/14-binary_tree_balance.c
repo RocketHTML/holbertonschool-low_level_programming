d645272aab80ff800ad2f820cbe75784aa97d531
root <madisononealburke@gmail.com>
2018-07-05 20:39:05 +0000

    inline height function

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -1,6 +1,26 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - returns height of tree
+ * @tree: pointer to tree
+ * Return: height of tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l;
+	size_t r;
+
+	if (tree == NULL)
+		return (0);
+	l = binary_tree_height(tree->left);
+	r = binary_tree_height(tree->right);
+	if (l > r)
+		return (l + 1);
+	else
+		return (r + 1);
+}
+
+/**
  * binary_tree_balance - returns the balance factor of the tree
  * @tree: pointer to the tree to measure
  * Return: balance factor, which is left height minus right height
