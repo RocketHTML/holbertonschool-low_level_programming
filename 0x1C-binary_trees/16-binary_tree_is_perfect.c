1d1454aebcadc4bfeaf9c95d55f9fe5545731ec2
root <madisononealburke@gmail.com>
2018-07-06 02:28:51 +0000

    checks if tree is perfect

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
 
+/**
+ * pft - recursively check if tree is perfect
+ * @tree: pointer to tree
+ * Return: height of tree or negative 1
+ */
 int pft(const binary_tree_t *tree)
 {
 	char status;
@@ -17,6 +22,11 @@ int pft(const binary_tree_t *tree)
 	return (-1);
 }
 
+/**
+ * binary_tree_is_perfect - returns whether tree is perfect or not
+ * @tree: pointer to tree
+ * Return: 1 for true, 0 for false
+ */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
