0da1e8a889888139af306a3990e9de875e9e0555
root <madisononealburke@gmail.com>
2018-07-06 19:14:51 +0000

    find parent sibling

---
+++ 0x1C-binary_trees/18-binary_tree_uncle.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - returns uncle of node
+ * @node: node to evaluate
+ * Return: sibling of parent node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t* parent;
+
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+
+	parent = node->parent;
+	if (parent->parent->left == parent)
+		return (parent->parent->right);
+	else
+		return (parent->parent->left);	
+}
